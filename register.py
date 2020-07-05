f1c3d9448bd67b035713b8d88116ff0100259fc6
DurianFarmer <kyuwookchai@gmail.com>
2020-07-06 08:27:48 +0900

    “Duriantest3

--- register.py
+++ register.py
@@ -1,55 +1,78 @@
 # register.py
 # register information of students, seats, clusters into DB
 
+# no.    path
+# 1      'usertable.csv'
+# 2      'clustertable.csv'
+# 3      'seattable.csv'
+
 import sqlite3
 import csv
 conn = sqlite3.connect('test.db')
 c= conn.cursor()
 def insert_from_csv(path, table):
-    c= conn.cursor()
-    if table =='1':
-        usertable = open(path,'r')
-        reader = csv.reader(usertable)
-        user=[]
-        for row in reader:
-            user.append(tuple(row))
-        c.executemany('''INSERT INTO users VALUES (?,?)''',user)
-        print('insert user table completed')
-        conn.commit()
-        conn.close()
-    elif table =='2':
-        clustertable = open('clustertable.csv','r')
-        reader = csv.reader(clustertable)
-        cluster = []
-        for row in reader:
-            cluster.append(tuple(map(int,row)))
-        
-        c.executemany('''INSERT INTO cluster VALUES (?,?,?)''',cluster)
-        print('insert cluster table completed')
-        conn.commit()
-        conn.close()
-    
-    elif table=='3':
-        seattable = open('seattable.csv','r')
-        reader= csv.reader(seattable)
-        seat = []
-        conv = lambda i : i or None
-        for row in reader:
-            try:
-                seat.append(((int(row[0]),conv(row[1]),int(conv(row[2])))))
-            except TypeError:
-                seat.append(((int(row[0]),conv(row[1]),conv(row[2]))))
-        
-        c.executemany('''INSERT INTO seat VALUES (?,?,?)''',seat)
-        print('insert cluster table completed')
-        conn.commit()
-        conn.close()
-    else:
-        print('error', ' ', 'please type 1 or 2 or 3' )
-        #todo raise error
+    try:
+        c= conn.cursor()
+        if table =='1':
+            usertable = open(path,'r')
+            reader = csv.reader(usertable)
+            user=[]
+            for row in reader:
+                user.append(tuple(row))
+            c.executemany('''INSERT INTO users VALUES (?,?)''',user)
+            print('insert user table completed')
+            conn.commit()
+            conn.close()
+            usertable.close()
+            flag = False
+
+        elif table =='2':
+            clustertable = open(path,'r')
+            reader = csv.reader(clustertable)
+            cluster = []
+            for row in reader:
+                cluster.append(tuple(map(int,row)))
             
-        
-    return
+            c.executemany('''INSERT INTO cluster VALUES (?,?,?)''',cluster)
+            print('insert cluster table completed')
+            conn.commit()
+            conn.close()
+            clustertable.close()
+            flag = False
+
+        elif table=='3':
+            seattable = open(path,'r')
+            reader= csv.reader(seattable)
+            seat = []
+            conv = lambda i : i or None
+            for row in reader:
+                try:
+                    seat.append(((int(row[0]),conv(row[1]),int(conv(row[2])))))
+                except TypeError:
+                    seat.append(((int(row[0]),conv(row[1]),conv(row[2]))))
+            
+            c.executemany('''INSERT INTO seat VALUES (?,?,?)''',seat)
+            print('insert cluster table completed')
+            conn.commit()
+            conn.close()
+            seattable.close()
+            flag = False
+
+        elif table == 'q':
+            print('goodbye')
+            flag = False
+
+        else:
+            print('error', ' ', 'please type 1 or 2 or 3' )
+            flag = True            
+
+    except Exception as e:
+        print('### ERROR!! ###')
+        print(e, '\n')
+        flag = True
+
+    finally:
+        return flag
 
 def insert_from_command():
     conv = lambda i : i or None
@@ -63,26 +86,35 @@ def insert_from_command():
     for row in cursor:
         print(row)
     c = conn.cursor()
+
+    flag = True
+    def ask_to_end():
+        print('Do you want to quit?  (y/n)')
+        answer = input()
+        if answer =='y':
+            conn.commit()
+            conn.close()
+            print('goodbye')
+            flag = False
+        else:
+            flag = True
+        return flag
+
     print("what table? user(1), cluster(2), seat(3)")
     table= input()
-    if table =='1':
-        while True:
+
+    while flag:
+        if table =='1':
             print('please type user pid')
             pid = input()
             print('please type user full name')
             full_name = input()
             result = (pid,full_name)
-            c.execute('''INSERT INTO users VALUES (?,?)''',result)
+            c.execute('''INSERT INTO users VALUES (?,?)''', result)
             print('insert completed')
-            print('Do you want to quit?  (y/n)')
-            answer = input()
-            if answer =='y':
-                conn.commit()
-                conn.close()
-                break
-            
-    elif table =='2':
-        while True:
+            flag = ask_to_end()
+                
+        elif table =='2':
             print('please type cluster id (cid)')
             cid = int(input())
             print('please type number_of_seat')
@@ -92,16 +124,9 @@ def insert_from_command():
             result = (cid, number_of_seat, number_owned)
             c.execute('''INSERT INTO cluster VALUES (?,?,?)''', result)
             print('insert completed')
-            print('Do you want to quit? (y/n)')
-            answer = input()
-            if answer =='y':
-                conn.commit()
-                conn.close()
-                break
-    
-            
-    elif table =='3':
-        while True:
+            flag = ask_to_end      
+                
+        elif table =='3':
             print('please type seat id (sid)')
             sid  = int(input())
             print('please type owner_id')
@@ -110,45 +135,53 @@ def insert_from_command():
             cluster_id = input()
             try:
                 cluster_id = int(cluster_id)
-            except ValueError:
+            except ValueError2:
                 print('cluster_id must be int')
             
             result = (sid,conv(owner_id),conv(cluster_id))
             c.execute('''INSERT INTO seat VALUES (?,?,?)''', result)
             print('insert completed')
-            print('Do you want to quit? (y/n)')
-            answer = input()
-            if answer =='y':
-                conn.commit()
-                conn.close()
-                break
-    else:
-        print('error')
-        #todo raise error
+            flag = ask_to_end
+
+        elif table == 'q':
+            print('goodbye')
+            flag = False
+            
+        else:
+            print('error', ' ', 'please type 1 or 2 or 3' )
+            flag = True
                 
-    # TODO: remaining insert process. you can modify overall interface of course.
+        # TODO: remaining insert process. you can modify overall interface of course.
 
     return
 
 
 def insert_interface():
-    print("csv(1) or command(2)?")
+    print("csv(1) or command(2)?  (cf. always type 'q' to exit!!)")
     mode = input()
     if mode=='1': #CSV
-        print("enter the file path")
-        path = input()
-        print("what table? user(1), cluster(2), seat(3)")
-        table = input()
-        insert_from_csv(path, table)
-        # TODO
-
+        flag = True
+        while flag:
+            print("enter the file path (ex: usertable.csv, ../usertable.csv, etc.)")
+            path = input()
+            if path == 'q':
+                print('goodbye')
+                break
+            print("what table? user(1), cluster(2), seat(3)")
+            table = input()
+            flag = insert_from_csv(path, table)
+            # TODO
 
     elif mode=='2': #COMMAND
         insert_from_command()
         # TODO
 
+    elif mode=='q':
+        print('goodbye')
+
     else:
-        print("error")
+        print('error', ' ', 'please type 1 or 2\n')
+        insert_interface()
         # TODO: raise error
 
 if __name__=='__main__':
--- search.py
+++ search.py
@@ -3,17 +3,37 @@
 import sqlite3
 conn = sqlite3.connect('test.db')
 c= conn.cursor()
-print('what do you want to see? all student table(1), all seat table(2), student with seat(3)')
-query = input()
-if query =='1':
-    for row in c.execute('''SELECT * FROM users'''):
-        print(row)
-elif query =='2':
-    for row in c.execute('''SELECT * FROM seat'''):
-        print(row)
-elif query =='3':
-    for row in c.execute('''SELECT * FROM users LEFT JOIN seat ON users.pid = seat.owner_id'''):
-        print(row)
-        
-else:
-    print('error')
+flag = True
+
+def ask_to_end():
+    print('Do you want to quit? (y/n)')
+    answer = input()
+    if answer =='y':
+        print('goodbye')
+        conn.commit()
+        conn.close()
+        flag = False
+    else:
+        flag = True
+    return flag
+                
+while flag:
+    print('what do you want to see? all student table(1), all seat table(2), student with seat(3)')
+    query = input()
+    if query =='1':
+        for row in c.execute('''SELECT * FROM users'''):
+            print(row)
+        flag = ask_to_end()
+    elif query =='2':
+        for row in c.execute('''SELECT * FROM seat'''):
+            print(row)
+        flag = ask_to_end()
+    elif query =='3':
+        for row in c.execute('''SELECT * FROM users LEFT JOIN seat ON users.pid = seat.owner_id'''):
+            print(row)
+        flag = ask_to_end()            
+    else:
+        print('error', ' ', 'please type 1 or 2 or 3' )
+        flag = True 
+
+
